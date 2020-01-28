783970d2a8f8cc6341c7c7586360b4d9bf0d7c6d
hungcat <hungcat4sheep@gmail.com>
2020-01-29 03:38:15 +0900

    refactoring

--- mc_ctl.py
+++ mc_ctl.py
@@ -26,21 +26,21 @@ GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
 GITHUB_URL = 'https://github.com'
 GITHUB_RAW_URL = 'https://raw.githubusercontent.com'
-SCRIPT_DIR = pathlib.Path(__file__).parent
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
 USAGE = '''
 Usage: {0} [create|backup|destroy|destroy_without_backup|help] [target]
     create: Create and serve minecraft server
         {0} create [world_repository] [version|LATEST|SNAPSHOT]
-        ex: {0} create hungcat/minecraft_world 1.14.4
+        ex: {0} create hungcat/minecraft-world 1.14.4
     backup: Back up current world to corresponding github repository
         {0} backup [world_repository]
-        ex: {0} backup hungcat/minecraft_world
+        ex: {0} backup hungcat/minecraft-world
     destroy: Destroy current world with backup
         {0} destroy [world_repository]
-        ex: {0} destroy hungcat/minecraft_world
+        ex: {0} destroy hungcat/minecraft-world
     destroy_without_backup: Destroy current world without backup
         {0} destroy_without_backup [world_repository]
-        ex: {0} destroy_without_backup hungcat/minecraft_world
+        ex: {0} destroy_without_backup hungcat/minecraft-world
     list: List running worlds
         {0} list
     help: Show this
@@ -78,21 +78,21 @@ def command_handler(args):
 
     try:
         if action == 'create':
-            print(emoji.emojize(':muscle: Creating server...', use_aliases=True))
+            print(_emoji(':muscle: Creating server...(first creation takes a bit time for downloading image)'))
             print(create_server(world_name, version))
         elif action == 'list':
-            print(emoji.emojize(':muscle: Listing server...', use_aliases=True))
+            print(_emoji(':muscle: Listing server...'))
             print(list_server())
         elif action == 'backup':
-            print(emoji.emojize(':muscle: Backuping world data...', use_aliases=True))
+            print(_emoji(':muscle: Backuping world data...'))
             print(backup_world(world_name))
         elif action == 'destroy':
-            print(emoji.emojize(':muscle: Backuping world data...', use_aliases=True))
+            print(_emoji(':muscle: Backuping world data...'))
             print(backup_world(world_name))
-            print(emoji.emojize(':muscle: Destroying server...', use_aliases=True))
+            print(_emoji(':muscle: Destroying server...'))
             print(destroy_server(world_name))
         elif action == 'destroy_without_backup':
-            print(emoji.emojize(':muscle: Destroying server...', use_aliases=True))
+            print(_emoji(':muscle: Destroying server...'))
             print(destroy_server(world_name))
         elif action == 'help':
             print(USAGE)
@@ -100,7 +100,7 @@ def command_handler(args):
             print('Invalid action: {}'.format(action))
             print(USAGE)
     except Exception as e:
-        print(emoji.emojize(':no_good: Error: {}'.format(e), use_aliases=True))
+        print(_emoji(':no_good: Error: {}'.format(e)))
 
     return
 
@@ -111,9 +111,9 @@ def list_server():
 
 def create_server(world_name='', version=''):
     try:
-        commands = _construct_droplet_docker_commands(world_name, version)
+        commands, version = _construct_droplet_docker_commands(world_name, version)
     except Exception as e:
-        return emoji.emojize(':no_good: Exit: {}'.format(e), use_aliases=True)
+        return _emoji(':no_good: Exit: {}'.format(e))
 
     private_key, public_key = _get_ssh_keys()
     droplet = _create_droplet(public_key, world_name)
@@ -127,16 +127,17 @@ def create_server(world_name='', version=''):
 
     if status == 0:
         print('Minecraft has waked up!')
-        message = emoji.emojize(':hammer_and_pick: Created instance: `{}`'.format(ip_address), use_aliases=True)
+        message = _emoji(':hammer_and_pick: Created instance: `{}`'.format(ip_address))
+        message = _emoji(':hammer_and_pick: Created minecraft_{} instance: `{}`'.format(version, ip_address))
     else:
         print('Minecraft couldn\'t wake up!')
-        print(emoji.emojize(':muscle: Destroying server...', use_aliases=True))
-        message = emoji.emojize(':cry: Failed to create server...', use_aliases=True)
+        print(_emoji(':muscle: Destroying server...'))
+        message = _emoji(':cry: Failed to create server...')
         try:
             destroy_server()
         except Exception as e:
-            print(emoji.emojize(':no_good: Error: {}'.format(e), use_aliases=True))
-            print(emoji.emojize(':cry: Please destroy this server yourself...', use_aliases=True))
+            print(_emoji(':no_good: Error: {}'.format(e)))
+            print(_emoji(':cry: Please destroy this server yourself...'))
 
     return message
 
@@ -156,7 +157,7 @@ def _construct_droplet_docker_commands(world_name, version):
             with urllib.request.urlopen(urllib.request.Request(version_url)) as res:
                 v = res.read().decode('utf-8')
         except urllib.request.URLError as e:
-            print(emoji.emojize(':information: MCCTL_VERSION.txt may not exists: {}'.format(e), use_aliases=True))
+            print(_emoji(':information: MCCTL_VERSION.txt may not exists: {}'.format(e)))
         if v != '':
             if version == '':
                 version = v
@@ -169,23 +170,23 @@ def _construct_droplet_docker_commands(world_name, version):
 
     commands.append('docker run -d -v /root/data:/data -e EULA=TRUE -e VERSION={} -e WORLD=/data/world --name minecraft -p 25565:25565 itzg/minecraft-server'.format(version))
 
-    return commands
+    return commands, version
 
 
 def backup_world(world_name=''):
     backup_url = _construct_github_url(world_name)
     if _test_github_url(backup_url) == False:
-        return emoji.emojize(':thinking_face: Unavailable repository: {}/{}'.format(GITHUB_URL, world_name), use_aliases=True)
+        return _emoji(':thinking_face: Unavailable repository: {}/{}'.format(GITHUB_URL, world_name))
 
     manager = digitalocean.Manager(token=DIGITALOCEAN_API_TOKEN)
     all_droplets = manager.get_all_droplets()
-    target = filter(lambda droplet: droplet.name == 'minecraft_{}'.format(world_name), all_droplets)
+    target = filter(lambda droplet: droplet.name == 'minecraft-{}'.format(world_name), all_droplets)
     if len(target) == 0:
-        target = filter(lambda droplet: droplet.name == 'minecraft_', all_droplets)
+        target = filter(lambda droplet: droplet.name == 'minecraft-', all_droplets)
         if len(target) == 0:
-            return emoji.emojize(':thinking_face: That world is not running', use_aliases=True)
+            return _emoji(':thinking_face: That world is not running')
         elif _yes_no_input('Overwrite {}/{} with running minecraft world?'.format(GITHUB_URL, world_name)) == False:
-            return emoji.emojize(':raised_hand: cannceled overwriting {}/{} with running minecraft world'.format(GITHUB_URL, world_name), use_aliases=True)
+            return _emoji(':raised_hand: cannceled overwriting {}/{} with running minecraft world'.format(GITHUB_URL, world_name))
     droplet = target[0]
 
     private_key = _get_ssh_keys()
@@ -204,22 +205,22 @@ def backup_world(world_name=''):
         'git push'
     ])
 
-    message = emoji.emojize(':rocket: Backuped world: {}/{}'.format(GITHUB_URL, world_name), use_aliases=True)
+    message = _emoji(':rocket: Backuped world: {}/{}'.format(GITHUB_URL, world_name))
     return message
 
 def destroy_server(world_name=''):
     manager = digitalocean.Manager(token=DIGITALOCEAN_API_TOKEN)
     all_droplets = manager.get_all_droplets()
-    target = filter(lambda droplet: droplet.name == 'minecraft_{}'.format(world_name), all_droplets)
+    target = filter(lambda droplet: droplet.name == 'minecraft-{}'.format(world_name), all_droplets)
     if len(target) == 0:
-        message = emoji.emojize(':thinking_face: That world is not running', use_aliases=True)
+        message = _emoji(':thinking_face: That world is not running')
         return message
     droplet = target[0]
 
     ip_address = _get_ip_address_of_droplet(droplet)
     droplet.destroy()
 
-    message = emoji.emojize(':boom: Destroyed instance: `{}`'.format(ip_address), use_aliases=True)
+    message = _emoji(':boom: Destroyed instance: `{}`'.format(ip_address))
     return message
 
 @retry(tries=30, delay=5)
@@ -245,7 +246,7 @@ def _exec_commands(client, commands, ignore_error=False):
 
 def _create_droplet(public_key, world_name=''):
     key_name = 'hungcat-mc-ctl-' + public_key[-7:]
-    droplet_name = 'minecraft_{}'.format(world_name)
+    droplet_name = 'minecraft-{}'.format(world_name)
     manager = digitalocean.Manager(token=DIGITALOCEAN_API_TOKEN)
 
     all_droplets = manager.get_all_droplets()
@@ -341,6 +342,9 @@ def _yes_no_input(message):
             return True
         elif choice in ['n', 'no']:
             return False
+
+def _emoji(mes):
+    return emoji.emojize(mes, use_aliases=True)
 
 if __name__ == '__main__':
     command_handler(sys.argv)
